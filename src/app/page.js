82c963625b7b397cc5bdcf9573a9d039da6f2fa0
udayan-majumder <udayanmajumder1983@gmail.com>
2025-08-17 17:15:20 +0530

    inital commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,55 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState, useEffect } from "react";
+import {AddLinkCards} from "../components/cardsComponent"
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [Categories, setCategories] = useState([
+    "Instagram",
+    "Youtube",
+    "Whatsapp",
+    "X",
+    "Secret",
+    "Others",
+  ]);
+  const [LinksDB, setLinksDB] = useState([]);
+  const [isCard, setisCard] = useState(false);
+  const  [SelectedCategory,setSelectedCategory] = useState("")
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const isDB = localStorage.getItem("LinkDB");
+    if (!isDB) {
+      localStorage.setItem("LinkDB", JSON.stringify(LinksDB));
+    } else {
+      setLinksDB(JSON.parse(isDB));
+    }
+  }, []);
+
+  useEffect(()=>{
+ localStorage.setItem("LinkDB",JSON.stringify(LinksDB))
+  },[LinksDB])
+
+  return (
+    <div className="Main-container">
+      <div className="sub-container">
+        <div className="Navbar">
+          <div className="Navbar-text">LinkSaver</div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {isCard ? (
+            <AddLinkCards selectedCatgeory={SelectedCategory} linkDB={LinksDB} setIsCard={setisCard} setlinksDB={setLinksDB}/>
+        ) : (
+          <div className="Categories-div">
+            {Categories?.map((items) => (
+              <div className="Category-cards-div">
+                  <button className="Category-cards" onClick={()=>{
+                    setSelectedCategory(items)
+                    setisCard(true)
+                  }}
+                    >{items}</button>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/cardsComponent.jsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { useState } from "react"
+
+export const AddLinkCards= ({selectedCatgeory,linkDB,setlinksDB,setIsCard})=>{
+   
+    const [TopicName,setTopicName] = useState("")
+    const [Link,setLink] = useState("")
+ 
+    return(
+        <div className="Main-LinkCard-Container">
+            <div className="LinkCards-Heading">{selectedCatgeory} Links</div>
+            <div className="Input-div">
+            <input placeholder="Topic Name" onChange={(e)=>{setTopicName(e.target.value)}} value={TopicName}/>
+            <input placeholder="Link" onChange={(e)=>{setLink(e.target.value)}} value={Link}/>
+            <button onClick={()=>{
+                if(TopicName.length>0 && Link.length>0){
+                 setlinksDB(prev=>[...prev,{"type":selectedCatgeory,"topicname":TopicName,"link":Link}])
+                setTopicName("")
+                setLink("")
+                }
+               
+            }}>Save</button>
+            </div>
+            <div className="Links-show-div">
+               {linkDB?.map((items)=>(
+               items?.type === selectedCatgeory ? <a href={items?.link} target="_blank">
+                {items?.topicname}
+               </a> : null
+               ))}
+               
+            </div>
+            <div className="Close-LinkCard">
+                <button onClick={()=>setIsCard(false)} >Close</button>
+            </div>
+            
+        </div>
+    )
+}
+
